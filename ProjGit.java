423b1782007ee879439ad71e0cf81c7b0d75f281
ShouanSushant <Shouans@sheridancollege.ca>
2025-09-29 23:47:37 -0400

    Added greeting message to test Git branching

--- ProjGit.java
+++ ProjGit.java
@@ -1,1 +1,1 @@
-"System.out.println('This is Git');" 
+'public class ProjGit {    public static void main(String[] args) {        System.out.println("Hello from Git Branch! This is Sushant testing Git workflows.");    }}' 
